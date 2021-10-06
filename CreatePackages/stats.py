a99edfffd33027fa220fa4faa0542f804cbc4583
therealmaxmouse <5930710+therealmaxmouse@users.noreply.github.com>
2021-10-06 14:06:50 -0400

    Update stats.py

--- CreatePackages/stats.py
+++ CreatePackages/stats.py
@@ -18,11 +18,3 @@ def median(numbers):
     else:
        mymedian = numbers[len(numbers) // 2]
     return mymedian
-
-Practice Exercise
-Create a new module named geometry and add to the mymath package.
-Create a module name geometry
-Add a function named area_of_rectangle that takes length and breadth as input and returns the area of a rectangle.
-Add a function named area_of_circle that takes radius as input and returns the area of a circle.
-Modify the __init__.py to include this module.
-Import and test the function area_of_circle from python terminal.
