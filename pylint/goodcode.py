3e9881111fc3a27bb87886e1411dce51092f880f
therealmaxmouse <5930710+therealmaxmouse@users.noreply.github.com>
2021-10-06 14:48:04 -0400

    Create goodcode.py

---
+++ pylint/goodcode.py
@@ -0,0 +1,9 @@
+"""This is a module docstring"""
+def add(number1, number2):
+    """this is a function docstring"""
+    return number1 + number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
