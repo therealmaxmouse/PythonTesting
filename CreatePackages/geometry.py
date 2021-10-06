e218eb3dee2591aa327f1311cae87ea3b98789d5
therealmaxmouse <5930710+therealmaxmouse@users.noreply.github.com>
2021-10-06 14:19:31 -0400

    Create geometry.py

---
+++ CreatePackages/geometry.py
@@ -0,0 +1,18 @@
+# Import math Library
+import math
+
+# Add a function named area_of_rectangle that takes length and breadth as input and returns the area of a rectangle.
+def area_of_rectangle(l,w):
+    return l * w
+
+# Add a function named area_of_circle that takes radius as input and returns the area of a circle.
+def area_of_circle(r):
+    return math.pi * r ** 2
+  
+# to run:
+## python3
+#>>> import mymath
+#>>> mymath.geometry.area_of_rectangle(325.535,4355.555)
+#1417885.5969250002
+#>>> mymath.geometry.area_of_circle(3)
+#28.274333882308138
